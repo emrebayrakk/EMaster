19ae6e216abc3999ce8de218f81b8b58aabfbe2a
Emre Bayrak <77586103+emrebayrakk@users.noreply.github.com>
2025-01-13 14:55:04 +0300

    minör

--- EMaster.Domain/Requests/ExpenseRequest.cs
+++ EMaster.Domain/Requests/ExpenseRequest.cs
@@ -2,7 +2,7 @@
 {
     public class ExpenseRequest
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public int CategoryID { get; set; }
         public decimal Amount { get; set; }
         public DateTime Date { get; set; } = DateTime.Now;
--- EMaster.Domain/Requests/IncomeRequest.cs
+++ EMaster.Domain/Requests/IncomeRequest.cs
@@ -2,7 +2,7 @@
 {
     public class IncomeRequest
     {
-        public int CategoryID { get; set; }
+        public int? CategoryID { get; set; }
         public decimal Amount { get; set; }
         public DateTime Date { get; set; } = DateTime.Now;
         public string? Description { get; set; }
