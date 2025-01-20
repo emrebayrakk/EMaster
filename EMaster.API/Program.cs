9f392135e296eb507f4bb3887a02dfca666d5d54
Emre Bayrak <77586103+emrebayrakk@users.noreply.github.com>
2025-01-20 17:54:56 +0300

    feat: add rate limiting to HTTP requests
    - Introduce rate limiting using `System.Threading.RateLimiting`
    - Configure rate limiter in `Program.cs` to allow 3 requests per 3 seconds
      per partition, determined by the `Host` header
    - Respond with 429 status code and JSON error message if rate limit is
      exceeded, including a retry-after message if available
    - Add `app.UseRateLimiter()` middleware to enforce rate limiting rules
    - Include `EMaster.Domain.Responses` namespace to support new functionality

--- EMaster.API/Program.cs
+++ EMaster.API/Program.cs
@@ -10,9 +10,11 @@ using EMaster.Data.Repositories;
 using EMaster.Data.Repositories.EntityFramework;
 using EMaster.Domain.Interfaces;
 using EMaster.Domain.Interfaces.EntityFramework;
+using EMaster.Domain.Responses;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
+using System.Threading.RateLimiting;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -56,6 +58,46 @@ builder.Services.AddCors(options =>
     });
 });
 
+builder.Services.AddRateLimiter(options =>
+{
+    options.GlobalLimiter = PartitionedRateLimiter.Create<HttpContext, string>(httpcontext =>
+    RateLimitPartition.GetFixedWindowLimiter(
+        partitionKey: httpcontext.Request.Headers.Host.ToString(),
+        factory: partition => new FixedWindowRateLimiterOptions
+        {
+            AutoReplenishment = true,
+            PermitLimit = 3,
+            Window = TimeSpan.FromMilliseconds(3000)
+        }
+    ));
+
+    options.OnRejected = async (context, token) =>
+    {
+        context.HttpContext.Response.StatusCode = 429;
+        ApiResponse<string> apiResponse = new ApiResponse<string>(
+                false,
+                429,
+                "Error",
+                ""
+            );
+
+        if (context.Lease.TryGetMetadata(MetadataName.RetryAfter, out var retryAfter))
+        {
+            apiResponse.Data = $"Ýstek sýnýr sayýsýna ulaþtýnýz. {retryAfter.TotalMinutes} dakika sonra tekrar deneyiniz.";
+            string jsonResponse = System.Text.Json.JsonSerializer.Serialize(apiResponse);
+            context.HttpContext.Response.ContentType = "application/json";
+            await context.HttpContext.Response.WriteAsync(jsonResponse, cancellationToken: token);
+        }
+        else
+        {
+            apiResponse.Data = "Ýstek sýnýrýna ulaþtýnýz. Daha sonra tekrar deneyin.";
+            string jsonResponse = System.Text.Json.JsonSerializer.Serialize(apiResponse);
+            context.HttpContext.Response.ContentType = "application/json";
+            await context.HttpContext.Response.WriteAsync(jsonResponse, cancellationToken: token);
+        }
+    };
+});
+
 MapsterConfiguration.ConfigureMappings();
 
 var app = builder.Build();
@@ -76,6 +118,7 @@ app.UseCors("AllowAll");
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseRateLimiter();
 
 app.MapControllers();
 
